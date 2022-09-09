c5c7d5e869a2c87d0c664b1ad0cfc1c1b3cb099f
Tobias Boogh <tobias@boogh.se>
2022-09-09 19:49:13 +0200

    Add search for swiftlint configuration file

--- Plugins/SwiftLintPlugin/SwiftLintPlugin.swift
+++ Plugins/SwiftLintPlugin/SwiftLintPlugin.swift
@@ -1,15 +1,23 @@
 import PackagePlugin
+import Foundation
 
 @main
 struct SwiftLintPlugin: BuildToolPlugin {
     func createBuildCommands(context: PackagePlugin.PluginContext, target: PackagePlugin.Target) async throws -> [PackagePlugin.Command] {
-        [
+        let fileManager = FileManager.default
+        // Possible paths where there may be a config file (root of package, target dir.)
+        let configurations: [Path] = [context.package.directory, target.directory]
+            .map { $0.appending("swiftlint.yml") }
+            .filter { fileManager.fileExists(atPath: $0.string) }
+
+        return try configurations.map {
             .buildCommand(
                 displayName: "SwiftLint",
                 executable: try context.tool(named: "swiftlint").path,
-                arguments: []
-            ),
-        ]
+                arguments: [
+                    "--config", $0
+                ])
+        }
     }
 }
 
@@ -19,13 +27,20 @@ import XcodeProjectPlugin
 extension SwiftLintPlugin: XcodeBuildToolPlugin {
 
     func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
-        [
+        let fileManager = FileManager.default
+        // Possible paths where there may be a config file (root of package, target dir.)
+        let configurations: [Path] = [context.xcodeProject.directory]
+            .map { $0.appending("swiftlint.yml") }
+            .filter { fileManager.fileExists(atPath: $0.string) }
+
+        return try configurations.map {
             .buildCommand(
                 displayName: "SwiftLint",
                 executable: try context.tool(named: "swiftlint").path,
-                arguments: []
-            )
-        ]
+                arguments: [
+                    "--config", $0
+                ])
+        }
     }
 }
 #endif
