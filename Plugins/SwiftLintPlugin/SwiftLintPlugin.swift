a1771accc87280a827fc32ebe5f7ef4a5f8c1b2a
Tobias Boogh <tobias@boogh.se>
2022-09-09 20:06:01 +0200

    Test paths

--- Plugins/SwiftLintPlugin/SwiftLintPlugin.swift
+++ Plugins/SwiftLintPlugin/SwiftLintPlugin.swift
@@ -28,12 +28,13 @@ extension SwiftLintPlugin: XcodeBuildToolPlugin {
 
     func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
         let fileManager = FileManager.default
-        // Possible paths where there may be a config file (root of package, target dir.)
+        // Possible paths where there may be a config file
         let configurations: [Path] = [context.xcodeProject.directory]
             .map { $0.appending("swiftlint.yml") }
             .filter { fileManager.fileExists(atPath: $0.string) }
-
-        return try configurations.map {
+        let configs: [Path]
+        = context.xcodeProject.filePaths.filter { $0.string.contains("swiftlint.yml")}
+        return try [configurations, configs].map {
             .buildCommand(
                 displayName: "SwiftLint",
                 executable: try context.tool(named: "swiftlint").path,
